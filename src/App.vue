e2422d7702582e241613aa61f08e3aee79885bda
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-21 14:39:38 -0300

    Trabalhando com Formulários

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
-<OptionsApi v-if="showComponent" />
-<CompositionApi v-if="showComponent" />
-
-<button @click="showComponent = !showComponent">Toggle</button>
+<Form />
 </template>
 
 <script setup>
-import OptionsApi from '@/components/LifeCycle/OptionsApi.vue'
-import CompositionApi from '@/components/LifeCycle/CompositionApi.vue'
-import { ref } from 'vue'
 
-const showComponent = ref(false)
+import Form from '@/components/Form/Form.vue'
 </script>
---
+++ src/components/Form/Form.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Form</h1>
+
+  <form action="" @submit.prevent="submitForm">
+    <label for="name">Nome</label>
+    <input type="text" name="name" id="name" v-model="form.name">
+    <br><br>
+    <label for="email">Email</label>
+    <input type="email" name="email" id="email" v-model="form.email">
+    <br><br>
+    <input type="submit">
+
+  </form>
+</template>
+
+<script setup>
+import { reactive } from 'vue'
+
+/**
+ * $user = User::query()->where('email', 'matheuspurgato@gmail.com')->first();
+ * return view('user.edit', ['user' => $user]) // blade
+ * return response()->json($user) // json
+ * return Inertia::render('Form.vue', ['user' => $user]) // Inertia
+ */
+
+/*const props = defineProps({
+  user: {
+    type: Object,
+    required: true,
+  }
+})*/
+
+const props = {
+  user: {
+    name: 'Matheus',
+    email: 'matheuspurgato@gmail.com'
+  }
+}
+
+const form = reactive({
+  name: props.user.name,
+  email: props.user.email
+})
+
+const submitForm = () => {
+  console.log('Formulário enviado com sucesso!')
+}
+</script>
