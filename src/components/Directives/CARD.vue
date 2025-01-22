bc1373312aeb09f40002354697e2d82dd41297c0
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-22 13:29:30 -0300

    Diretiva v-slot

---
+++ src/components/Directives/CARD.vue
@@ -0,0 +1,35 @@
+<template>
+
+<!-- # = v-slot -->
+<!-- : = v-model -->
+<!-- @ = v-on -->
+  <div>
+    <header>
+      <slot name="header"></slot>
+    </header>
+  </div>
+  <div class="title">
+    <slot name="title"></slot>
+  </div>
+  <div>
+    <main>
+      <slot name="content"></slot>
+    </main>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style scoped>
+header {
+  color: red
+}
+
+.title {
+  background: yellow;
+  color: blue;
+}
+main{
+  color: green;
+}
+</style>
---
+++ src/components/Directives/VSLOT.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Diretiva v-slot</h1>
+
+  <button>Salvar</button>
+</template>
+<script setup>
+
+</script>
