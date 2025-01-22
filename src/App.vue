ee59d6cdaa2ccd9345cab565ed0e3c3f9713ef18
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-22 16:44:11 -0300

    Recursos avançados Emit

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,26 @@
 <template>
-<!--
-<Vif />
-<VSHOW />
-<VTEXT />
-<VHTML />
-<VFOR />
-<VSLOT />
-<CARD>
-<template #header>
-<h1>Cabeçalho do meu card</h1>
-</template>
-<template #title>
-<h2>Título do meu card</h2>
-</template>
-<template #content>
-<p>aqui está o conteúdo do meu card</p>
-</template>
-</CARD>
-<VPRE />
-<VCLOAK />
-<VONCE />
-<VMEMO />
-<Composition/>
-<Options />
--->
-<CompositionCard
-  title="Este é o título do meu card"
-  :body="'Este é o conteudo do meu card'"
-  :access="1230"
-  :status="true"
-  format="user"
-/>
+  <CompositionCard
+    :title="card.title"
+    :access="card.access"
+    :status="card.status"
+    :format="card.format"
+    @updateTitle="changeTitle"
+  />
+
 </template>
 
 <script setup>
-/*
-import Vif from '@/components/Directives/VIF.vue'
-import VSHOW from '@/components/Directives/VSHOW.vue'
-import VTEXT from '@/components/Directives/VTEXT.vue'
-import VHTML from '@/components/Directives/VHTML.vue'
-import VFOR from '@/components/Directives/VFOR.vue'
-import VSLOT from '@/components/Directives/VSLOT.vue'
-import CARD from '@/components/Directives/CARD.vue'
-import VPRE from '@/components/Directives/VPRE.vue'
-import VCLOAK from '@/components/Directives/VCLOAK.vue'
-import VONCE from '@/components/Directives/VONCE.vue'
-import VMEMO from '@/components/Directives/VMEMO.vue'
-import Composition from '@/components/Register/Composition.vue'
-import Options from '@/components/Register/Options.vue'
-*/
-
 import CompositionCard from '@/components/Props/CompositionCard.vue'
+import { reactive } from 'vue'
+const card = reactive({
+  title: 'Este é o título do meu card',
+  access: 1230,
+  status: true,
+  format: 'user'
+})
 
+const changeTitle = (data) => {
+  //card.title = 'Novo título'
+  card.title = data
+}
 </script>
--- src/components/Props/CompositionCard.vue
+++ src/components/Props/CompositionCard.vue
@@ -1,48 +1,17 @@
 <template>
   <h1>Título do meu card: {{props.title}}</h1>
+  <button @click.prevent="emitEventForUpdateTitle">Alteraro titulo</button>
   <p>Conteúdo do meu card: {{props.body}}</p>
   <p>Quantidade de acessos: {{props.access}}</p>
   <p>Status: {{props.status}}</p>
   <p>Formato: {{props.format}}</p>
-  <hr>
-  <CoursesCard :title="props.title" :status="props.status" :access="props.access" />
-</template>
+ </template>
 <script setup>
+const emit = defineEmits(['updateTitle'])
 
-// const props = defineProps([
-//   'title',
-//   'body',
-//   'access',
-//   'status'
-// ])
-//
-// const props = defineProps({
-//   title: String,
-//   body: String,
-//   access: Number,
-//   status: Boolean,
-// })
-//
-// const props = defineProps({
-//   title: {
-//     type: String,
-//     required: true,
-//   },
-//   body: {
-//     type: String,
-//     default: 'Valor Padrão',
-//   },
-//   access: {
-//     type: Number || String,
-//     default: 0,
-//   },
-//   status: {
-//     type: Boolean,
-//     required: true,
-//   },
-// })
-
-import CoursesCard from '@/components/Props/CoursesCard.vue'
+const emitEventForUpdateTitle = () => {
+  emit('updateTitle', 'O Purga é foda demais!')
+}
 
 const props = defineProps({
   title: {
