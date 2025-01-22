51cb244917764433eb239c4afc47ac980ab6c65f
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-22 13:41:10 -0300

    Diretiva v-pre

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,35 @@
 <template>
-  <!--
-  <Vif />
-  <VSHOW />
-  <VTEXT />
-  <VHTML />
-  <VFOR />
-  -->
-  <VSLOT />
-  <CARD>
-    <template #header>
-      <h1>Cabeçalho do meu card</h1>
-    </template>
-    <template #title>
-      <h2>Título do meu card</h2>
-    </template>
-    <template #content>
-      <p>aqui está o conteúdo do meu card</p>
-    </template>
-  </CARD>
-
+<!--
+<Vif />
+<VSHOW />
+<VTEXT />
+<VHTML />
+<VFOR />
+<VSLOT />
+<CARD>
+<template #header>
+<h1>Cabeçalho do meu card</h1>
+</template>
+<template #title>
+<h2>Título do meu card</h2>
+</template>
+<template #content>
+<p>aqui está o conteúdo do meu card</p>
+</template>
+</CARD>
+-->
+  <VPRE />
 </template>
 
 <script setup>
 /*
 import Vif from '@/components/Directives/VIF.vue'
 import VSHOW from '@/components/Directives/VSHOW.vue'
-
 import VTEXT from '@/components/Directives/VTEXT.vue'
 import VHTML from '@/components/Directives/VHTML.vue'
 import VFOR from '@/components/Directives/VFOR.vue'
-
-*/
 import VSLOT from '@/components/Directives/VSLOT.vue'
 import CARD from '@/components/Directives/CARD.vue'
+*/
+import VPRE from '@/components/Directives/VPRE.vue'
 </script>
---
+++ src/components/Directives/VPRE.vue
@@ -0,0 +1,13 @@
+<template>
+<h1>Diretiva v-pre</h1>
+  <p>o nome do usupario é: {{user}}</p>
+  <p v-pre>o nome do usupario é: {{ Purgato }}</p>
+</template>
+
+<script setup>
+const user = '{{ Purgato }}'
+</script>
+
+<style scoped>
+
+</style>
