a9d3d2174ad0200c8d18ba64d1b324192134d0d6
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-23 12:29:08 -0300

    Recursos avançados Transition

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <p>Saída do comando Date: {{ new Date() }}</p>
-  <p>Data formatada {{ paramDate}}</p>
+  <button @click="toggle = !toggle">Toggle</button>
+  <Transition
+    enter-from-class=" opacity-0 translate-y-1"
+    enter-to-class="opacity-100 translate-y-0"
+    enter-active-class="transition ease-out duration-200"
+    leave-from-class="opacity-100 translate-y-0"
+    leave-to-class="opacity-0 translate-y-1"
+    leave-active-class="transition ease-in duration-150">
+    <div v-if="toggle">
+      <TransitionComponent />
+    </div>
+  </Transition>
 </template>
 
 <script setup>
+import TransitionComponent from '@/components/Transition/TransitionComponent.vue'
+import { ref } from 'vue'
 
-import { useToDateFormat } from './components/Composable/useToDateFormat.js'
-const {paramDate} = useToDateFormat(new Date())
+const toggle = ref(true)
 </script>
---
+++ src/components/Transition/TransitionComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="div">
+    <p class="paragrafo"> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi cumque dignissimos error iste magni maxime nam officiis, placeat provident temporibus. Accusamus aliquam amet architecto deleniti distinctio dolore esse, facere fugiat fugit in ipsa iure labore odit pariatur quibusdam reprehenderit vel, velit veritatis, vitae voluptatibus. Adipisci architecto, asperiores at delectus dolore, ex illum laudantium libero maiores nesciunt nisi officiis omnis optio perferendis perspiciatis sunt temporibus ullam, ut velit voluptate! At autem blanditiis distinctio eaque enim incidunt ipsa laudantium odio quis quos, recusandae repellendus reprehenderit, temporibus voluptate voluptatibus! Alias, cum eius facere, illo laboriosam minima molestias natus numquam odit quaerat reiciendis sit. </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.div{
+  padding: 30px;
+}
+
+.paragrafo{
+  background-color: #666;
+  padding: 30px;
+  color: #fff;
+  font: bold 15px Arial;
+}
+</style>
