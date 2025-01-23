c2313cb305db7d8ab389baa09829f82d85f61ab2
Purgato96 <matheus_purgato@hotmail.com.com>
2025-01-22 21:39:31 -0300

    Recursos avançados Suspense

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>
-<Users />
+<SuspenseComponent />
 </template>
 
 <script setup>
-import Users from '@/components/ProvideInject/Users.vue'
-import { provide } from 'vue'
-
-const users = [
-{
-  id: 1,
-  name: 'Matheus',
-},
-{
-  id: 2,
-  name: 'Ana'
-},
-]
-
-provide('users', users)
+import SuspenseComponent from '@/components/Suspense/SuspenseComponent.vue'
 </script>
---
+++ src/components/Suspense/SuspenseComponent.vue
@@ -0,0 +1,28 @@
+<template>
+<h1>Suspense Component</h1>
+  <div v-if="loaded === false">Carregando...</div>
+  <div v-else>Listagem de usuários completa</div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const loaded = ref(false)
+
+onMounted( async () => {
+  loaded.value = false
+  await listUser()
+  loaded.value = true
+})
+
+const listUser = async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+        console.log('Chegou aqui')
+        resolve()
+      },
+      3000)
+  })
+}
+
+</script>
